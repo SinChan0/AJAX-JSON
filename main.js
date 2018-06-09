61901be7dbf5d5a92d72b52c09324f261253ee8d
sinchan0 <nitinprajapatishahdol@gmail.com>
2018-06-09 14:36:05 +0530

    impelemented the parsing... search is still remaining

--- main.js
+++ main.js
@@ -1,54 +1,30 @@
-var data = document.getElementById("tbl");
-var btn = document.getElementById("btn");
-document.getElementById('count').innerHTML=0;
-document.getElementById("myInput").style.display = 'none';``
+var btn = document.getElementById("btn"); 
+var table = document.getElementById("tbl");
 
 btn.addEventListener("click", function(){
     var myRequest = new XMLHttpRequest();
-    myRequest.open('GET','https://sinchan0.github.io/AJAX-JSON/data.json', true);
+    myRequest.open('GET','https://sinchan0.github.io/AJAX-JSON/data.json');
     myRequest.onload = function(){
-        var myObject = JSON.parse(myRequest.responseText);
-        console.log(myObject);
-        renderHTML(myObject);
+        var myData = JSON.parse(myRequest.responseText);
+        renderHTML( myData );
     };
     myRequest.send();
-});
+} );
 
-function renderHTML(myObject){
+function renderHTML(data){
     var htmlString = "";
-    data.insertAdjacentHTML('beforeend' , htmlString);
     var i = 0;
-    for(i=0; i<myObject.lenght; i++){
-        htmlString += "<tr><td>" + myObject[i].Name + "</td><td>" + myObject[i].Version + "</td></tr>" + myObject[i].API;
+    
+    for (i=0; i<data.length; i++)
+    {
+        htmlString += "<tr><td>" + data[i].Name + "</td><td>" + data[i].Version + "</td><td>" + data[i].API + " </td></tr>";
     }
-    if(i==myObject.length)
+    table.insertAdjacentHTML('beforeend', htmlString);
+    if(i==data.length)
 	{
 		document.getElementById('count').innerHTML=i;
 		document.getElementById('btn').style.display='none';
 		document.getElementById('myInput').style.display='block';
     }
 }
-
-function myFunction() {
-    var input, filter, table, tr, td, i,j;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("info");
-    tr = table.getElementsByTagName("tr");
-    var count=0;
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-          td1 = tr[i].getElementsByTagName("td")[1];
-          if (td+td1) {
-            if ((td.innerHTML.toUpperCase().indexOf(filter)+td1.innerHTML.toUpperCase().indexOf(filter)) > -2) {
-              tr[i].style.display = ""; 
-              count++;
-                 document.getElementById('count').innerHTML=count;
-            } else {
-              tr[i].style.display = "none";
-              document.getElementById('count').innerHTML=count;
-            }    
-      }
-    }
-  }
 
