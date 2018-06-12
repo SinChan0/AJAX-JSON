ec95bbd4bae936fb5517b8b6ef15e05e78727444
sinchan0 <nitinprajapatishahdol@gmail.com>
2018-06-13 00:56:47 +0530

    search works

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var btn = document.getElementById("btn"); 
 var table = document.getElementById("tbl");
+document.getElementById('count').innerHTML=0;
+document.getElementById("myInput").style.display = 'none';
 
 btn.addEventListener("click", function(){
     var myRequest = new XMLHttpRequest();
@@ -28,3 +30,26 @@ function renderHTML(data){
     }
 }
 
+function myFunction() {
+    var input, filter, table, tr, td, td1, td2, i, j;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tbl");
+    tr = table.getElementsByTagName("tr");
+    var count=0;
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        td1 = tr[i].getElementsByTagName("td")[1];
+        td2 = tr[i].getElementsByTagName("td")[2];
+          if (td+td1) {
+            if ((td.innerHTML.toUpperCase().indexOf(filter)+td1.innerHTML.toUpperCase().indexOf(filter)+td2.innerHTML.toUpperCase().indexOf(filter)) > -3) {
+              tr[i].style.display = ""; 
+              count++;
+                 document.getElementById('count').innerHTML=count;
+            } else {
+              tr[i].style.display = "none";
+              document.getElementById('count').innerHTML=count;
+            }    
+      }
+    }
+}
